dd14d3afd53ba407953316d9a7670cb9ea4cf84a
Md. Iqbal Hossain <ekra.ict22@gmail.com>
2019-02-28 11:44:06 +0600

    domain create

---
+++ contactApp/src/main/java/com/mycompany/contactapp/domain/Contact.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.contactapp.domain;
+
+/**
+ *
+ * @author WALTON
+ */
+public class Contact {
+    
+    public int contactId;  //PK
+    public int userId;  //FK
+    private String name;
+    private String phone;
+    private String address;
+    private String email;
+    private String remark;
+
+    public Contact() {
+    }
+
+    public int getContactId() {
+        return contactId;
+    }
+
+    public void setContactId(int contactId) {
+        this.contactId = contactId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+    
+    
+}
